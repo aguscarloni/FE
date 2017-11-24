f7b33acc87149b7639811ca9a0ed8652d6aaae7a
german <german@german-670Z5E>
2017-11-24 11:26:46 -0300

    prueba

--- FINAL/BackEnd.c
+++ FINAL/BackEnd.c
@@ -4,4 +4,4 @@
  * and open the template in the editor.
  */
 
-//Frontend.c
+//BackEnd.c
---
+++ FINAL/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: german
+ *
+ * Created on November 24, 2017, 11:19 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//maaaaaaaain
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
